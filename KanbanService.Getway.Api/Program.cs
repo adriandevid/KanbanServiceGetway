c2adf6258aa460a17c81507ef9924ad608336dcf
Adrian Devid Menezes Santos <AdrianMS@seed.se.gov>
2022-10-04 08:45:46 -0300

    implementação de swagger com ocelot e consul hashicorp

--- KanbanService.Getway.Api/Program.cs
+++ KanbanService.Getway.Api/Program.cs
@@ -5,11 +5,16 @@ using KanbanService.Getway.Api.Models;
 using Consul;
 using ProjetoService.Configurations;
 using Ocelot.Middleware;
+using Microsoft.OpenApi.Models;
+using MMLib.Ocelot.Provider.AppConfiguration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 builder.Services.AddSingleton<IHostedService, ServiceRecoverConfig>();
 builder.Services.Configure<GetwayConfiguration>(builder.Configuration.GetSection("GetwayService"));
@@ -27,22 +32,26 @@ builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
-builder.Services.AddOcelot(builder.Configuration).AddConsul().AddCacheManager(x => x.WithDictionaryHandle());
+builder.Services.AddOcelot(builder.Configuration).AddAppConfiguration().AddConsul().AddCacheManager(x => x.WithDictionaryHandle());
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-///<summary>
-/// Validação para auto preservação
-/// </summary>
-app.MapGet("/health", () =>
-{
-    return "OK";
-})
-.WithName("health");
-app.UseOcelot();
+/////<summary>
+///// Validação para auto preservação
+///// </summary>
+//app.MapGet("/health", () =>
+//{
+//    return "OK";
+//})
+//.WithName("health");
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseStaticFiles();
+app.UseSwaggerForOcelotUI()
+.UseOcelot()
+.Wait();
 
-app.UseHttpsRedirection();
+
+//app.UseHttpsRedirection();
 app.Run();
