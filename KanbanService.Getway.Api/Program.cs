1b07bd6eb8636e05360d4a38d6daf6f9afd6f90a
AdrianDevid <adriandevid36@gmail.com>
2022-10-03 21:04:02 -0300

    configurações finais para api gatway

--- KanbanService.Getway.Api/Program.cs
+++ KanbanService.Getway.Api/Program.cs
@@ -4,14 +4,9 @@ using Ocelot.Cache.CacheManager;
 using KanbanService.Getway.Api.Models;
 using Consul;
 using ProjetoService.Configurations;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
-
-builder.Services.AddOcelot(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,17 +14,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IHostedService, ServiceRecoverConfig>();
 builder.Services.Configure<GetwayConfiguration>(builder.Configuration.GetSection("GetwayService"));
 builder.Services.Configure<ConsulConfiguration>(builder.Configuration.GetSection("Consul"));
-builder.Services.AddOcelot(builder.Configuration).AddConsul().AddCacheManager(x => x.WithDictionaryHandle());
 
 var consulAddress = builder.Configuration.GetSection("Consul")["Url"];
 
 builder.Services.AddSingleton<IConsulClient, ConsulClient>(provider =>
-    new ConsulClient(config => {
+    new ConsulClient(config =>
+    {
         config.Address = new Uri(consulAddress);
     }));
 
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
-
+builder.Services.AddOcelot(builder.Configuration).AddConsul().AddCacheManager(x => x.WithDictionaryHandle());
 
 var app = builder.Build();
 
@@ -41,6 +39,7 @@ app.MapGet("/health", () =>
     return "OK";
 })
 .WithName("health");
+app.UseOcelot();
 
 app.UseSwagger();
 app.UseSwaggerUI();
