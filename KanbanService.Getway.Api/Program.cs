b15e7250d146f0644ad5f368f9acc9ee2f9a2b16
Adrian Devid Menezes Santos <AdrianMS@seed.se.gov>
2022-10-05 07:38:03 -0300

    new changes

--- KanbanService.Getway.Api/Program.cs
+++ KanbanService.Getway.Api/Program.cs
@@ -5,16 +5,12 @@ using KanbanService.Getway.Api.Models;
 using Consul;
 using ProjetoService.Configurations;
 using Ocelot.Middleware;
-using Microsoft.OpenApi.Models;
 using MMLib.Ocelot.Provider.AppConfiguration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-});
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IHostedService, ServiceRecoverConfig>();
 builder.Services.Configure<GetwayConfiguration>(builder.Configuration.GetSection("GetwayService"));
@@ -28,30 +24,15 @@ builder.Services.AddSingleton<IConsulClient, ConsulClient>(provider =>
         config.Address = new Uri(consulAddress);
     }));
 
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath).AddJsonFile("ocelot.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
 
 builder.Services.AddOcelot(builder.Configuration).AddAppConfiguration().AddConsul().AddCacheManager(x => x.WithDictionaryHandle());
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-/////<summary>
-///// Validação para auto preservação
-///// </summary>
-//app.MapGet("/health", () =>
-//{
-//    return "OK";
-//})
-//.WithName("health");
-
 app.UseSwagger();
 app.UseStaticFiles();
-app.UseSwaggerForOcelotUI()
-.UseOcelot()
-.Wait();
+app.UseSwaggerForOcelotUI().UseOcelot().Wait();
 
-
-//app.UseHttpsRedirection();
 app.Run();
